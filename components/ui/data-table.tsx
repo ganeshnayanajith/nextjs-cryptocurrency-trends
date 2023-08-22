e07d5068955bef5d76186d64f461858db732e8e5
Ganesh Nayanajith <32520964+ganeshnayanajith@users.noreply.github.com>
2023-08-22 12:03:50 +0530

    refactoring

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -23,8 +23,8 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 
 interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[]
-  data: TData[],
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
   searchKey: string;
 }
 
